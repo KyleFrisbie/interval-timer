8ff2775f99c135e86f5baf14adf1edc9749a918b
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2019-01-15 21:23:45 -0700

    added simple timer

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import clock from 'clock';
+import document from "document";
+
+import { msToTime } from '../common/utils'
+
+
+const startTime = Date.now();
+const timer = document.getElementById('timer');
+
+clock.granularity = 'seconds';
+
+clock.ontick = (event) => {
+    timer.text = msToTime(Date.now() - startTime);
+}
---
+++ common/utils.js
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/a/19700358/4488232
+export function msToTime(duration) {
+  let seconds = parseInt((duration / 1000) % 60);
+  let minutes = parseInt((duration / (1000 * 60)) % 60);
+  let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
+}
